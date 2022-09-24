36b2b934116f3b959baf43a744778e8517f88fb6
Denis Yurovsky <moon.wave578@gmail.com>
2022-09-24 19:37:53 +0300

    initial commit

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Page not found</h2>
+  <router-link to="/teams">Move to teams</router-link>
+</template>
---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="this.$router.push('/teams')">Confirm</button>
+  <ul>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  beforeRouteEnter(to, from, next) {
+    console.log(to, from);
+    next();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
